970fa8940c796c7cc163d1dc0833853502a30a47
Alex Caldarone <cldrna_coding.is235@aleeas.com>
2024-03-26 14:38:55 +0100

    changed folder name + added makefile

--- data_gathering_cleaning_pipeline.sh
+++ data_gathering_cleaning_pipeline.sh
@@ -6,6 +6,7 @@ if [ ! -d "data" ]; then
     mkdir data
 fi
 
+# create raw data directory
 if [ ! -d "data/raw" ]; then
     mkdir data/raw/
 fi
@@ -15,28 +16,29 @@ if [ ! -d "data/raw/extraurbani_pdf" ]; then
     mkdir data/raw/extraurbani_pdf
 fi
 
-# avvia script scraper.py (rendi generale directory anziché tenere directory del mio computer)
+# script scraper.py
 echo "Downloading bus timetables..."
-python3 scripts/data_gathering/scraper.py
+python3 src/data_gathering/scraper.py
 echo "Finished downloading"
 
-# elimina file corrispondente a linea19_1
+# deleting malformed file
 echo "Deleting a malformed file..."
 rm data/raw/extraurbani_pdf/LineaE019_1.pdf
 echo "Done"
 
+# creating clean data directory
 if [ ! -d "data/clean" ]; then
     mkdir data/clean
 fi
 
-# avvia script pdf_reader.py
+# script pdf_reader.py
 echo "Reading data from pdf files..."
-python3 scripts/data_gathering/pdf_reader.py
+python3 src/data_gathering/pdf_reader.py
 echo "Done"
 
-# avvia script json_cleaner.py (cambia output in csv anzichè txt)
+# script json_cleaner.py
 echo "Cleaning data in json files..."
-python3 scripts/data_gathering/json_cleaner.py
+python3 src/data_gathering/json_cleaner.py
 echo "Finished"
 
 echo "The finished edge list is in data/clean/edge_list.csv"
